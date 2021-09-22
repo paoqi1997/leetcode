1ccb17c744ae335101d7962f563f6f7bdbc6f0ee
paoqi1997 <30751379+paoqi1997@users.noreply.github.com>
2021-09-22 22:05:42 +0800

    leetcode: 存在重复元素 III

--- leetcode/T-220.cpp
+++ leetcode/T-220.cpp
@@ -5,6 +5,7 @@
 
 #include <cmath>   // fabs
 #include <cstdlib> // abs
+#include <set>
 #include <vector>
 
 using namespace std;
@@ -27,6 +28,36 @@ public:
                 if (fabs(x - y) <= t && abs(i - j) <= k) {
                     return true;
                 }
+            }
+        }
+
+        return false;
+    }
+
+    bool containsNearbyAlmostDuplicate_v2(vector<int>& nums, int k, int t) {
+        set<long> s;
+
+        // abs(nums[i] - nums[j]) <= t
+        // 两个元素的最大差是 t
+
+        for (int i = 0; i < nums.size(); ++i) {
+            // nums[i] - nums[j] <= t
+            // nums[j] >= nums[i] - t
+            auto it = s.lower_bound((long) nums[i] - (long) t);
+
+            // nums[j] - nums[i] <= t
+            // nums[j] <= nums[i] + t
+            if (it != s.end() && *it <= ((long) nums[i] + (long) t)) {
+                return true;
+            }
+
+            // 作为 nums[j]
+            s.insert(nums[i]);
+
+            // i - j <= k
+            // j >= i - k
+            if (s.size() > k) {
+                s.erase(nums[i - k]);
             }
         }
 
